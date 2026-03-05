c36f7d190ad148142ef50697ae071196d6dfbde4
mrln033 <merlin.merzhin.lesage@gmail.com>
2026-03-05 16:12:32 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbzDThy2g6d0Chn4R1NkmJYHc-lngx8jaQjDndTfOimjqhhyf5TwLUeHJ0nJ3W88gJzibA/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwOzsGoftmeL2aZUrefVaWsr2NAUcvXA44_CdDObR3Ym6h3U2P36AGzmGFS30YhzyPKnQ/exec";
