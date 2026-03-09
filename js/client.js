5db65c79652c8cb6b41613361092d32d04a3cc26
mrln033 <merlin.merzhin.lesage@gmail.com>
2026-03-09 12:39:39 +0100

    .

--- js/client.js
+++ js/client.js
@@ -107,7 +107,7 @@ function displayMembresActifs(list) {
 			<td>${m.date ? m.date + " (" + m.entreeCount + ")" : ""}</td>
 			<td>${calcAnciennete(m.date)}</td>
 			<td class="regle-cell">
-				${m.IDDiscord ? '<span"><img src="images/discord-32.png" alt="Discord"></span> + ' : ''}
+				${m.IDDiscord ? '<span"><img src="images/icon-discord.png" alt="Discord"></span> + ' : ''}
 				${m.regleSoc ? '<span class="regle-ok">Oui</span>' : '<span class="regle-ko">Non</span>'}
 			</td>
 		`;
