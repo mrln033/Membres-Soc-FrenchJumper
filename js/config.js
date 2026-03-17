e0d8690892152e59e4aad89fbab8832d87fb5961
mrln033 <merlin.merzhin.lesage@gmail.com>
2026-03-17 13:20:59 +0100

    .

--- js/config.js
+++ js/config.js
@@ -1,1 +1,6 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbyVkS2XRgNZP9pSMCw7vtZoUfFRMTpPXBBJ-HFoRq8GM_1BldT7AP2_Zv587s6do9QS/exec";
+
+const WH_NOTIF_RH = "https://discord.com/api/webhooks/1483422952786493514/9sMzKb1YgTVwKM2jTUbaZ5DnlI0iPxUTE7mF_bavHckfoVFYPj4SIj6DCf_uJqPl4ap0"
+
+
+
