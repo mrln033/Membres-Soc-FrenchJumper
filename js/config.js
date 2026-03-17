10eb1204882e59752a15cd5a67fcc2fb3a5cc992
mrln033 <merlin.merzhin.lesage@gmail.com>
2026-03-17 13:27:14 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,4 +3,3 @@ const API_URL = "https://script.google.com/macros/s/AKfycbyVkS2XRgNZP9pSMCw7vtZo
 const WH_NOTIF_RH = "https://discord.com/api/webhooks/1483422952786493514/9sMzKb1YgTVwKM2jTUbaZ5DnlI0iPxUTE7mF_bavHckfoVFYPj4SIj6DCf_uJqPl4ap0"
 
 
-
