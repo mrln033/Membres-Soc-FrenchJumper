892d08b168cd3dc581bfd51b0af13d19d827f29b
mrln033 <merlin.merzhin.lesage@gmail.com>
2026-03-18 14:39:57 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/library/d/15s4990apxaNb6Pj4mHcUZ1pixmsjOQ3QAY-trmGeqw7aOBEC5mRFp4fq/123";
+const API_URL = "https://script.google.com/macros/s/AKfycbzdHMtJqw8SEPJdVMpDprTNF6ygPkYBlGh1UxtxCfRkhMhXXua7jbK-eq12f8_Kc-gmGQ/exec";
 
 const WH_NOTIF_RH = "https://discord.com/api/webhooks/1483422952786493514/9sMzKb1YgTVwKM2jTUbaZ5DnlI0iPxUTE7mF_bavHckfoVFYPj4SIj6DCf_uJqPl4ap0"
 
