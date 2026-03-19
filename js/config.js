99668651e8f182bbc352d9bf99e9f7940d628e0f
mrln033 <merlin.merzhin.lesage@gmail.com>
2026-03-19 16:38:32 +0100

    .

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
-    Admin.init();
-});
-
 const API_URL = "https://script.google.com/macros/s/AKfycbzwUDJBSb--QOij9g1nnh9Ex8nXEa3iwedfNtMEZCbPbB-ruoyuQIeP6u-fAsGdrZ3W8Q/exec";
 
 const WH_NOTIF_RH = "https://discord.com/api/webhooks/1483422952786493514/9sMzKb1YgTVwKM2jTUbaZ5DnlI0iPxUTE7mF_bavHckfoVFYPj4SIj6DCf_uJqPl4ap0"
 
+const isAdmin = sessionStorage.getItem("admin") === "true";
 
+if (isAdmin) {
+    console.log("Mode admin activé");
+}
